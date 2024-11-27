021cc23aed42c8f9260b4dc56e0d0f09a589ccbc
Arnaud Poletto <arnaud.poletto@epfl.ch>
2024-11-27 12:58:22 +0100

    update livesal config

--- runs/livesal_graph_dhf1k.sh
+++ runs/livesal_graph_dhf1k.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=livesal
-#SBATCH --output=/scratch/izar/poletto/logs/log_livesal_temporal_graph_salicon_%j.out
-#SBATCH --error=/scratch/izar/poletto/logs/log_livesal_temporal_graph_salicon_%j.err
+#SBATCH --output=/scratch/izar/poletto/logs/log_livesal_graph_dhf1k_%j.out
+#SBATCH --error=/scratch/izar/poletto/logs/log_livesal_graph_dhf1k_%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1  # Number of GPUs per node
 #SBATCH --gres=gpu:1         # Adjust based on available GPUs
@@ -17,6 +17,6 @@ source /home/poletto/venvs/pdm/bin/activate
 
 cd /home/poletto/code
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-srun python src/livesal/livesal_train.py -c /home/poletto/code/config/livesal/global_graph_salicon.yml
+srun python src/livesal/livesal_train.py -c /home/poletto/code/config/livesal/graph_dhf1k.yml
 
 
