fix output normalization in models

src/models/livesal.py
@@ -28,6 +28,7 @@ class LiveSAL(nn.Module):
         with_graph_directional_kernels: bool,
         with_depth_information: bool,
         with_global_output: bool,
+        eps: float = 1e-6,
     ) -> None:
         super(LiveSAL, self).__init__()
 
@@ -45,6 +46,7 @@ class LiveSAL(nn.Module):
         self.with_graph_directional_kernels = with_graph_directional_kernels
         self.with_depth_information = with_depth_information
         self.with_global_output = with_global_output
+        self.eps = eps
 
         # Get normalization parameters for encoder/estimator inputs
         self.register_buffer(
@@ -238,10 +240,9 @@ class LiveSAL(nn.Module):
         x: torch.Tensor,
         mean: torch.Tensor,
         std: torch.Tensor,
-        eps: float = 1e-6,
     ) -> torch.Tensor:
         x = x.clone()
-        normalized_x = (x - mean) / (std + eps)
+        normalized_x = (x - mean) / (std + self.eps)
 
         return normalized_x
     

src/models/tempsal.py
@@ -43,7 +43,7 @@ class TempSAL(nn.Module):
         self.freeze_encoder = freeze_encoder
         self.freeze_temporal_pipeline = freeze_temporal_pipeline
         self.hidden_channels_list = hidden_channels_list
-        self.with_global_output = 
+        self.with_global_output = with_global_output
         self.eps = eps
 
         # Get normalization parameters for encoder inputs
@@ -105,10 +105,9 @@ class TempSAL(nn.Module):
         x: torch.Tensor,
         mean: torch.Tensor,
         std: torch.Tensor,
-        eps: float = 1e-6,
     ) -> torch.Tensor:
         x = x.clone()
-        normalized_x = (x - mean) / (std + eps)
+        normalized_x = (x - mean) / (std + self.eps)
 
         return normalized_x
 

src/models/vidas.py
@@ -259,6 +259,7 @@ class ViDaS(nn.Module):
         saliency_out_channels: int,
         attention_out_channels: int,
         with_depth_information: bool,
+        eps: float = 1e-6,
     ) -> None:
         super(ViDaS, self).__init__()
 
@@ -269,6 +270,7 @@ class ViDaS(nn.Module):
         self.saliency_out_channels = saliency_out_channels
         self.attention_out_channels = attention_out_channels
         self.with_depth_information = with_depth_information
+        self.eps = eps
 
         self.image_encoder = ViDaSEncoder(
             input_channels=input_channels,
@@ -357,10 +359,9 @@ class ViDaS(nn.Module):
         x: torch.Tensor, 
         mean: torch.Tensor, 
         std: torch.Tensor,
-        eps: float = 1e-6,
     ) -> torch.Tensor:
         x = x.clone()
-        normalized_x = (x - mean) / (std + eps)
+        normalized_x = (x - mean) / (std + self.eps)
 
         return normalized_x
     