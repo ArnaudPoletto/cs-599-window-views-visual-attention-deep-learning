fix output normalization in models

src/models/livesal.py
@@ -244,6 +244,15 @@ class LiveSAL(nn.Module):
         normalized_x = (x - mean) / (std + eps)
 
         return normalized_x
+    
+    def _normalize_spatial_dimensions(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.clone()
+        batch_size, channels , height, width = x.size()
+        x = x.view(batch_size, channels, -1)
+        x = x / (x.max(dim=2, keepdim=True)[0] + self.eps)
+        x = x.view(batch_size, channels, height, width)
+
+        return x
 
     def _get_image_features_list(
         self, x: torch.Tensor, is_image: bool
@@ -462,11 +471,12 @@ class LiveSAL(nn.Module):
             depth_decoded_features=depth_decoded_features,
         )
         temporal_output = self.sigmoid(temporal_features)
-        temporal_output = temporal_output / (temporal_output.max(dim=(2, 3), keepdim=True)[0] + self.eps)
+        temporal_output = self._normalize_spatial_dimensions(temporal_output)
 
         # Get global output if required
         if self.with_global_output:
-            global_output = self.final_global_layer(temporal_features).squeeze(1)
+            global_output = self.final_global_layer(temporal_features)
+            global_output = self._normalize_spatial_dimensions(global_output).squeeze(1)
         else:
             global_output = None
 

src/models/tempsal.py
@@ -111,6 +111,15 @@ class TempSAL(nn.Module):
         normalized_x = (x - mean) / (std + eps)
 
         return normalized_x
+
+    def _normalize_spatial_dimensions(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.clone()
+        batch_size, channels , height, width = x.size()
+        x = x.view(batch_size, channels, -1)
+        x = x / (x.max(dim=2, keepdim=True)[0] + self.eps)
+        x = x.view(batch_size, channels, height, width)
+
+        return x
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -129,7 +138,7 @@ class TempSAL(nn.Module):
         # Decode temporal features and get temporal output
         temporal_features = self.temporal_decoder(encoded_features_list)
         temporal_output = self.sigmoid(temporal_features)
-        temporal_output = temporal_output / (temporal_output.max(dim=(2, 3), keepdim=True)[0] + self.eps)
+        temporal_output = self._normalize_spatial_dimensions(temporal_output)
 
         # Decode global features and get global output with spatio-temporal mixing module if needed
         if self.with_global_output:
@@ -138,8 +147,8 @@ class TempSAL(nn.Module):
                 encoded_features_list=encoded_features_list,
                 temporal_features=temporal_features,
                 global_features=global_features,
-            ).squeeze(1)
-            global_output = global_output / (global_output.max(dim=(1, 2), keepdim=True)[0] + self.eps)
+            )
+            global_output = self._normalize_spatial_dimensions(global_output).squeeze(1)
         else:
             global_output = None
 

src/models/vidas.py
@@ -51,6 +51,15 @@ class DSAM(nn.Module):
 
         self.relu = nn.ReLU()
 
+    def _normalize_spatial_dimensions(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.clone()
+        batch_size, channels , height, width = x.size()
+        x = x.view(batch_size, channels, -1)
+        x = x / (x.max(dim=2, keepdim=True)[0] + self.eps)
+        x = x.view(batch_size, channels, height, width)
+
+        return x
+
     def forward(
         self, x: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
@@ -354,6 +363,15 @@ class ViDaS(nn.Module):
         normalized_x = (x - mean) / (std + eps)
 
         return normalized_x
+    
+    def _normalize_spatial_dimensions(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.clone()
+        batch_size, channels , height, width = x.size()
+        x = x.view(batch_size, channels, -1)
+        x = x / (x.max(dim=2, keepdim=True)[0] + self.eps)
+        x = x.view(batch_size, channels, height, width)
+
+        return x
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         if self.with_depth_information:
@@ -382,6 +400,7 @@ class ViDaS(nn.Module):
             decoded_maps = torch.cat([image_decoded_maps, depth_decoded_maps], dim=1)
         else:
             decoded_maps = image_decoded_maps
-        global_output = self.final_layer(decoded_maps).squeeze(1)
+        global_output = self.final_layer(decoded_maps)
+        global_output = self._normalize_spatial_dimensions(global_output).squeeze(1)
 
         return None, global_output