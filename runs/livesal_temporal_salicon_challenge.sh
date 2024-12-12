9d4a1a76c22d9ec6cb46a9fd1b3eafd661a667ce
Arnaud Poletto <arnaud.poletto@epfl.ch>
2024-12-12 18:02:45 +0100

    update nnodes in sbatch

--- runs/livesal_temporal_salicon_challenge.sh
+++ runs/livesal_temporal_salicon_challenge.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ltsc
 #SBATCH --output=/scratch/izar/poletto/logs/log_livesal_temporal_salicon_challenge_%j.out
 #SBATCH --error=/scratch/izar/poletto/logs/log_livesal_temporal_salicon_challenge_%j.err
-#SBATCH --nodes=4
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=2
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=8
@@ -17,6 +17,6 @@ source /home/poletto/venvs/pdm/bin/activate
 
 cd /home/poletto/code
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-srun python src/livesal/livesal_train.py -c /home/poletto/code/config/livesal/temporal_salicon_challenge.yml -n 4
+srun python src/livesal/livesal_train.py -c /home/poletto/code/config/livesal/temporal_salicon_challenge.yml -n 2
 
 
